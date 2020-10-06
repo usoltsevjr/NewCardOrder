afce01acf46ab5af61d10cd8bb98da96dbf228ff
usoltsevjr <usoltsevjr@gmail.com>
2020-10-06 13:06:38 +0300

    Updated test's name, CSS selectors and added Annotation

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -1,4 +1,5 @@
 import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
@@ -7,9 +8,13 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class CardOrderTest {
 
+    @BeforeEach
+    void SetUpAll(){
+        open("http://localhost:9999");
+    }
+
     @Test
     void shouldSendCorrectForm() {
-        open("http://localhost:9999");
         $("[data-test-id=name] input").setValue("Антон Антонов");
         $("[data-test-id=phone] input").setValue("+79217895546");
         $("[data-test-id=agreement]").click();
@@ -18,28 +23,25 @@ public class CardOrderTest {
     }
 
     @Test
-    void shouldInputCorrectName() {
-        open("http://localhost:9999");
+    void shouldNotSubmitIfNameIsIncorrect() {
         $("[data-test-id=name] input").setValue("Anton Antonov");
         $("[data-test-id=phone] input").setValue("+79217895546");
         $("[data-test-id=agreement]").click();
         $("[type=button]").click();
-        $(".input__sub").shouldHave(exactText("Имя и фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
-    void shouldInputCorrectPhone() {
-        open("http://localhost:9999");
+    void shouldNotSubmitIfPhoneIsIncorrect() {
         $("[data-test-id=name] input").setValue("Антон Антонов");
         $("[data-test-id=phone] input").setValue("+792178955460");
         $("[data-test-id=agreement]").click();
         $("[type=button]").click();
-        $$(".input__sub").last().shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $$("[data-test-id=phone].input_invalid .input__sub").last().shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
     void shouldSubmitAgreement() {
-        open("http://localhost:9999");
         $("[data-test-id=name] input").setValue("Антон Антонов");
         $("[data-test-id=phone] input").setValue("+79217895546");
         $("[type=button]").click();
@@ -48,7 +50,6 @@ public class CardOrderTest {
 
     @Test
     void shouldNotSubmitWithoutData() {
-        open("http://localhost:9999");
         $("[data-test-id=agreement]").click();
         $("[type='button']").click();
         $("[data-test-id=name].input_invalid .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
